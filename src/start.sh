83d3813489f0820efcbd7c867ab9769981754082
ultimatech-cn <142652112+ultimatech-cn@users.noreply.github.com>
2025-05-13 01:18:17 +0800

    修改启动命令

--- src/start.sh
+++ src/start.sh
@@ -5,7 +5,7 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
 echo "worker-comfyui: Starting ComfyUI"
-comfy launch -- --listen 0.0.0.0 --port 8188 &
+python /root/comfy/ComfyUI/main.py --disable-auto-launch --listen 0.0.0.0 --port 8188 &
 
 echo "worker-comfyui: Starting RunPod Handler"
 python -u /root/rp_handler.py
