8e72e0d807187a0defe0c9ae3b809c121ed7b35b
ultimatech-cn <142652112+ultimatech-cn@users.noreply.github.com>
2025-05-14 00:41:37 +0800

    import timm.layers

--- src/start.sh
+++ src/start.sh
@@ -4,6 +4,8 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
+python -c 'import timm; from timm.layers import LayerNorm2d; print(\"Success!\")'
+
 echo "worker-comfyui: Starting ComfyUI"
 python /root/comfy/ComfyUI/main.py --disable-auto-launch --listen 0.0.0.0 --port 8188 &
 
