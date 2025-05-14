dc4e6a6ef0bfcfd29404f1d6454c28419887ca4c
ultimatech-cn <142652112+ultimatech-cn@users.noreply.github.com>
2025-05-14 11:38:10 +0800

    初始化 venv

--- src/start.sh
+++ src/start.sh
@@ -4,10 +4,10 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-python -c 'import timm; from timm.layers import LayerNorm2d; print("Success!")'
+source /venv/bin/activate
 
 echo "worker-comfyui: Starting ComfyUI"
-python /root/comfy/ComfyUI/main.py --disable-auto-launch --listen 0.0.0.0 --port 8188 &
+/venv/bin/python /root/comfy/ComfyUI/main.py --disable-auto-launch --listen 0.0.0.0 --port 8188 &
 
 echo "worker-comfyui: Starting RunPod Handler"
-python -u /root/rp_handler.py
+/venv/bin/python -u /root/rp_handler.py
